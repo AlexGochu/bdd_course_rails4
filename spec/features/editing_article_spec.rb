dbaf9968c5b2a269a68857b160bd3ccca301363e
Oleksandr Hochu <gochualex@gmail.com>
2018-01-31 22:35:37 +0000

    Implement edit article feature

---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature "Editing an article" do
+  
+  before do
+    @article = Article.create(title: "Title One", body: "Body of article one")
+  end
+  
+  scenario 'User updates an article' do
+    
+    visit "/"
+    
+    click_link @article.title
+    click_link "Edit article"
+    
+    fill_in "Title", with: "Updated title"
+    fill_in "Body", with: "Updated body"
+    
+    click_button "Update Article"
+    
+    expect(page).to have_content("Article has been updated")
+    expect(page.current_path).to eq(article_path(@article.id)) 
+  end
+  
+  scenario 'User fail to update an article' do
+    visit "/"
+    
+    click_link @article.title
+    click_link "Edit article"
+    
+    fill_in "Title", with: ""
+    fill_in "Body", with: "Updated body of article"
+    
+    click_button "Update Article"
+    
+    expect(page).to have_content("Article has not been updated")
+    expect(page.current_path).to eq(article_path(@article.id)) 
+  end
+  
+  
+end
